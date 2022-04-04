b825fed65a4c54cb2f0c4d4f1962d1c40f962cb1
mamamun009 <mamamun1999@gmail.com>
2022-04-04 22:02:07 +0600

    rendering html page

--- calc/views.py
+++ calc/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request): 
-    return HttpResponse("Hello World");
+def home(request):
+    return render(request, "home.html", {'name': 'Username'})
